2efa5243cdd7ef61a5c25794cf09ff206ca536b4
张天翼 <932881657@qq.com>
2023-05-18 16:59:06 +0800

    Signed-off-by: 张天翼 <932881657@qq.com>

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args){
         int[] a = {1, 2, 3};
-        System.out.println(a[1]);
-
+        System.out.println(a[2]);
+        System.out.print(a[1]);
     }
 }
